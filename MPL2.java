bd2a6363b13b1fc49e76fec451cee9b0700c441b
Amy Ru <amru@calpoly.edu>
2020-04-29 23:01:11 -0700

    Create MPL2.java

---
+++ MPL2.java
@@ -0,0 +1,27 @@
+// Input / Output
+val = stdin.nextInt();
+System.out.print("Today's Word-Of-The-Day is: " + word);
+System.out.println("first is " + first + " second = " + second);
+
+// Casting
+(double) distance / speed
+(double) children / families
+
+// Arithmetic Operations
+small + large
+endingTime - startingTime
+pricePerCase * 12
+totalWeight / quantity
+principal % divisor
+price / 100
+price % 100
+System.out.print(price / 100 + " dollars and " + price % 100 + " cents");
+(12 + 40) / 2
+(width1 * length1) + (width2 * length2)
+(double) (tickets1 + tickets2 + tickets3) / (class1 + class2 + class3)
+gradAge = matricAge + 4;
+bridgePlayers = bridgePlayers + 4;
+profits = profits * 10;
+total = total + amount;
+strawsOnCamel ++;
+timer --;
